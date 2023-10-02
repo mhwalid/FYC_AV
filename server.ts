62364a62254862c9dac0feae3ab359a03d5e8c2d
Basile2121 <basile.regnault@gmail.com>
2023-10-02 19:12:55 +0200

    Initialise commit

---
+++ server.ts
@@ -0,0 +1,19 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import todoRouter from "./routes/todo.ts";
+
+const app = new Application();
+const port: number = 8080;
+
+app.use(todoRouter.routes());
+app.use(todoRouter.allowedMethods());
+app.use(oakCors({ origin: "*" }));
+
+app.addEventListener("listen", ({ secure, hostname, port }) => {
+    const protocol = secure ? "https://" : "http://";
+    const url = `${protocol}${hostname ?? "localhost"}:${port}`;
+    console.log(`Listening on: ${port}`);
+});
+
+await app.listen({ port });
+export default app;
