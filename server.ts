a4c70cc6a3af177095bcf9e265f096a3b87b7e3d
Basile2121 <basile.regnault@gmail.com>
2024-02-09 17:43:31 +0100

    Ajout du template pour la vidéo

--- server.ts
+++ server.ts
@@ -2,20 +2,12 @@ import { Application, Router, oakCors, config } from "./deps.ts";
 import appRouter from "./routes/app/index.ts";
 import adminRouter from "./routes/admin/index.ts";
 import authRouter from "./routes/auth/authRouter.ts";
-import RequestLimitMiddleware from "./middlewares/check-all-requests.ts"
 
 const env = config();
 const { PORT, HOSTNAME } = env;
 
 const app = new Application();
 const router = new Router();
-
-const maxRequests = 100; // Nombre maximal de requêtes autorisées
-const requestDuration = 60 * 1000; // Durée de la fenêtre de requêtes en millisecondes (1 minute)const listener = Deno.listen({ hostname: "localhost", port: 8080 });
-
-// Création du middleware avec les paramètres personnalisés
-const requestLimitMiddleware = RequestLimitMiddleware("server", maxRequests, requestDuration);
-app.use(requestLimitMiddleware);
 
 // Utilisation des routers
 router.use("/app", appRouter.routes());
